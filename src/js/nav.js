03354d4269620f01c5ab33d6807b443800f18d7d
Vincent Boutour <vincent.boutour@gmail.com>
2016-07-04 22:30:40 +0200

    Changing to google maps api

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,18 +1,21 @@
 var toggleLink = document.getElementById('toggleMenu');
 var navElement = document.getElementsByTagName('nav')[0];
-function handleNav(forceHide) {
+
+function handleNav(show) {
   const navClasses = navElement.classList;
-  if (forceHide || navClasses.contains('displayed')) {
-    navClasses.remove('displayed');
-  } else {
+  if (show) {
     navClasses.add('displayed');
+  } else {
+    navClasses.remove('displayed');
   }
 }
+
 function handleClick(event) {
   if (navElement.contains(event.target)) {
       return;
   }
-  handleNav(!toggleLink.contains(event.target));
+  handleNav(toggleLink.contains(event.target));
 }
+
 document.addEventListener('click', handleClick);
 document.addEventListener('touchstart', handleClick);
