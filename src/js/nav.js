5f1f69e5058db7c50fde43f426b3cbcdfe12cea2
Vincent Boutour <vincent.boutour@gmail.com>
2016-07-07 11:48:10 +0000

    Revert "Update nav.js"
    This reverts commit 38bf925eb9e09f29d9757b202d241005e752f428.

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,10 +1,21 @@
-var navElement = document.getElementById('navMenu');
+var toggleLink = document.getElementById('toggleMenu');
+var navElement = document.getElementsByTagName('nav')[0];
+
+function handleNav(show) {
+  const navClasses = navElement.classList;
+  if (show) {
+    navClasses.add('displayed');
+  } else {
+    navClasses.remove('displayed');
+  }
+}
 
 function handleClick(event) {
   if (navElement.contains(event.target)) {
       return;
   }
-  location.hash = '';
+  handleNav(toggleLink.contains(event.target));
 }
 
+document.addEventListener('click', handleClick);
 document.addEventListener('touchstart', handleClick);
