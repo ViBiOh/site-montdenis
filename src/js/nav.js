38bf925eb9e09f29d9757b202d241005e752f428
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-07-07 13:33:12 +0200

    Update nav.js

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,21 +1,10 @@
-var toggleLink = document.getElementById('toggleMenu');
-var navElement = document.getElementsByTagName('nav')[0];
-
-function handleNav(show) {
-  const navClasses = navElement.classList;
-  if (show) {
-    navClasses.add('displayed');
-  } else {
-    navClasses.remove('displayed');
-  }
-}
+var navElement = document.getElementById('navMenu');
 
 function handleClick(event) {
   if (navElement.contains(event.target)) {
       return;
   }
-  handleNav(toggleLink.contains(event.target));
+  location.hash = '';
 }
 
-document.addEventListener('click', handleClick);
 document.addEventListener('touchstart', handleClick);
