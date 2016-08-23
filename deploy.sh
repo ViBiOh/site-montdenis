3b1091a24ad7664b431bfd99e89f1b74756f436b
Vincent <ViBiOh@users.noreply.github.com>
2016-08-23 13:10:09 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-export DOMAIN=pension-montdenis.fr
-export PROJECT_NAME=montdenis
+set -e
+
+function readVariableIfRequired() {
+  if [ -z "${!1}" ]; then
+    read -p "${1}=" $1
+  else
+    echo "${1}="${!1}
+  fi
+}
+
+if [ ${#} -eq 0 ]; then
+  echo Usage ${0} [DOMAIN] [PROJECT_NAME]
+fi
+
+DOMAIN=${1}
+readVariableIfRequired "DOMAIN"
+
+PROJECT_NAME=${2}
+readVariableIfRequired "PROJECT_NAME"
 
 docker-compose -p ${PROJECT_NAME} pull
 docker-compose -p ${PROJECT_NAME} up -d --force-recreate
 
 docker rmi `docker images --filter dangling=true -q 2>/dev/null` 2>/dev/null
-
