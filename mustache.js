837674af950aea1550a59cf3394252f1d71340f2
Vincent Boutour <vincent.boutour@gmail.com>
2016-09-12 21:50:59 +0200

    Continuing work on mustache

--- mustache.js
+++ mustache.js
@@ -1,4 +1,9 @@
 const Mustache = require('mustache');
+const glob = require('glob')
+const fs = require('fs');
+const utils = require('js-utils');
+
+const asyncReadFile = utils.asyncifyCallback(fs.readFile);
 
 const options = require('yargs')
   .reset()
@@ -11,18 +16,11 @@ const options = require('yargs')
   .options('partials', {
     alias: 'p',
     required: false,
-    array: true,
     type: 'String',
     describe: 'Partials'
   })
-  .options('json', {
-    alias: 'j',
-    required: false,
-    type: 'String',
-    describe: 'JSON containing values'
-  })
-  .options('input', {
-    alias: 'i',
+  .options('template', {
+    alias: 't',
     required: true,
     type: 'String',
     describe: 'Input file'
@@ -30,3 +28,17 @@ const options = require('yargs')
   .help('help')
   .strict()
   .argv;
+
+const partials = options.partials ? glob.sync(options.partials) : [];
+
+glob(options.template, {}, (err, templates) => {
+  templates.forEach(template => {
+    const allFiles = [];
+    allFiles.push(asyncReadFile(template.replace(/[^\/]*?\.html/gmi, 'mustache.json'), 'utf-8'));
+    allFiles.push(asyncReadFile(template, 'utf-8'));
+
+    Promise.all(allFiles).then(values => {
+      console.log(Mustache.render(values[1], values[0]));
+    });
+  });
+});
