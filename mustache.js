1893642f633719ebbb3c08f8fb24054ae4179786
Vincent Boutour <vincent.boutour@gmail.com>
2016-09-19 19:01:30 +0200

    Changing googleplus and inline css

--- mustache.js
+++ mustache.js
@@ -156,7 +156,7 @@ new Promise((resolve, reject) => {
   Promise.all(requiredPromises).then((required) => {
     const partials = required[0];
     partials.inlineJs = `<script type="text/javascript">${required[1]}</script>`;
-    partials.inlineCss = `<style type="text/css">${required[2]}</script>`;
+    partials.inlineCss = `<style type="text/css">${required[2]}</style>`;
 
     glob(options.template, {}, (error, templates) => {
       handleError(error, reject);
