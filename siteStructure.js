7c84946876147748ea8849442ca4f5b3aae4569b
Vincent <ViBiOh@users.noreply.github.com>
2016-09-26 14:45:22 +0200

    Update siteStructure.js

--- siteStructure.js
+++ siteStructure.js
@@ -55,8 +55,7 @@ function jsonPromise(json) {
 }
 
 function sitemapConverter(data) {
-  return 
-  `<url>
+  return `<url>
      <loc>${data.url}</loc>
      <changefreq>weekly</changefreq>
      <priority>1.00</priority>
@@ -64,8 +63,7 @@ function sitemapConverter(data) {
 }
 
 function sitemapStructure(urls) {
-  return 
-  `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
