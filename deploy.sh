a7862fc9cbea8a6b3322ec4e59b23e8d942cbe23
Vincent Boutour <bob@vibioh.fr>
2016-10-27 19:24:40 +0200

    Adding hotDeploy

--- deploy.sh
+++ deploy.sh
@@ -10,19 +10,27 @@ function readVariableIfRequired() {
   fi
 }
 
-function docker-compose-deploy() {
+function docker-compose-hot-deploy() {
   PROJECT_NAME=${1}
   readVariableIfRequired "PROJECT_NAME"
 
   DOMAIN=${2}
   readVariableIfRequired "DOMAIN"
 
-  export DOMAIN=${DOMAIN}
+  SERVICE_NAME=${3}
+  readVariableIfRequired "SERVICE_NAME"
   
+  export DOMAIN=${DOMAIN}
+
+  existingContainer=`docker-compose -p ${PROJECT_NAME} ps | grep ${SERVICE_NAME} | awk '{print $1}'`
+
   docker-compose -p ${PROJECT_NAME} pull
-  docker-compose -p ${PROJECT_NAME} stop
-  docker-compose -p ${PROJECT_NAME} rm -f -v
-  docker-compose -p ${PROJECT_NAME} up -d --force-recreate
+  docker-compose -p ${PROJECT_NAME} scale ${SERVICE_NAME}=2
+
+  echo "Waiting 5 seconds to start..."
+  sleep 5
+  docker stop ${existingContainer}
+  docker rm -f -v ${existingContainer}
   
   docker rmi `docker images --filter dangling=true -q 2>/dev/null` 2>/dev/null
 }
@@ -38,5 +46,5 @@ readVariableIfRequired "PROJECT_URL"
 rm -rf ${PROJECT_NAME}
 git clone ${PROJECT_URL} ${PROJECT_NAME}
 cd ${PROJECT_NAME}
-docker-compose-deploy ${PROJECT_NAME} ${3}
+docker-compose-hot-deploy ${PROJECT_NAME} ${3} ${4}
 
