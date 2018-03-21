975155bade36814855e90dfdb4dfc16af6a8db45
Vincent Boutour <bob@vibioh.fr>
2018-03-21 21:33:40 +0100

    Moving async image loading into its own script

---
+++ src/js/async_img_load.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function() {
+  function onImgLoad(id, img, throbber) {
+    return function() {
+      document.getElementById('figure-' + id).replaceChild(img, throbber);
+    }
+  }
+
+  var throbbers = document.querySelectorAll('.throbber');
+  for (var i = throbbers.length - 1; i >= 0; i--) {
+    var throbber = throbbers[i];
+
+    throbber.style.display = 'inline-block';
+
+    var img = new Image();
+    img.onload = onImgLoad(throbber.getAttribute('data-id'), img, throbber);
+    img.src = throbber.getAttribute('data-src');
+    img.alt = throbber.getAttribute('data-alt');
+    img.srcset = throbber.getAttribute('data-srcset');
+  }
+}, false);
