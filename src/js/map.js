475af73340f61104b426c250ee7dc9aa9d542fbb
Vincent Boutour <bob@vibioh.fr>
2018-09-15 20:41:27 +0200

    Conditionning map initialization as presence in dom

--- src/js/map.js
+++ src/js/map.js
@@ -1,15 +1,17 @@
-mapboxgl.accessToken =
-  'pk.eyJ1IjoidmliaW9oIiwiYSI6ImNqMzFmbTFoOTAwMWMycW82YXppbGRidXAifQ.bb6xyGsr1kQW3UozHS2Pmw';
+if (mapboxgl && document.getElementById('map')) {
+  var coordinates = [2.956476579635819, 48.86757827445496];
 
-var coordinates = [2.956476579635819, 48.86757827445496];
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoidmliaW9oIiwiYSI6ImNqMzFmbTFoOTAwMWMycW82YXppbGRidXAifQ.bb6xyGsr1kQW3UozHS2Pmw';
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v10',
-  center: coordinates,
-  zoom: 11,
-  scrollZoom: false,
-});
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: coordinates,
+    zoom: 11,
+    scrollZoom: false,
+  });
 
-map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-right');
-new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-right');
+  new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+}
