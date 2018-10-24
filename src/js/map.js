e817ca6a6b80c8cc35384757317c5e826bfe8daa
Vincent Boutour <bob@vibioh.fr>
2018-10-24 18:32:52 +0200

    charging map only on page load

--- src/js/map.js
+++ src/js/map.js
@@ -1,17 +1,19 @@
-if (window.mapboxgl && document.getElementById('map')) {
-  var coordinates = [2.956476579635819, 48.86757827445496];
+window.addEventListener('load', function() {
+  if (window.mapboxgl && document.getElementById('map')) {
+    var coordinates = [2.956476579635819, 48.86757827445496];
 
-  mapboxgl.accessToken =
-    'pk.eyJ1IjoidmliaW9oIiwiYSI6ImNqMzFmbTFoOTAwMWMycW82YXppbGRidXAifQ.bb6xyGsr1kQW3UozHS2Pmw';
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoidmliaW9oIiwiYSI6ImNqMzFmbTFoOTAwMWMycW82YXppbGRidXAifQ.bb6xyGsr1kQW3UozHS2Pmw';
 
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: coordinates,
-    zoom: 11,
-    scrollZoom: false,
-  });
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: coordinates,
+      zoom: 11,
+      scrollZoom: false,
+    });
 
-  map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-right');
-  new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
-}
+    map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-right');
+    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  }
+});
