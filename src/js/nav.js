2eff3f33c2fff4e2520aa43bdd5dae9623d454e9
Vincent Boutour <bob@vibioh.fr>
2018-10-25 12:53:23 +0200

    fixing nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,22 +1,25 @@
-const toggleButton = document.getElementById('toggleMenu');
-const navElement = document.getElementsByTagName('nav')[0];
+window.addEventListener('load', () => {
+  const toggleButton = document.getElementById('toggleMenu');
+  const navElement = document.getElementsByTagName('nav')[0];
 
-function handleNav(target) {
-  const navClasses = navElement.classList;
-  if (!target || navClasses.contains('displayed')) {
-    navClasses.remove('displayed');
-    toggleButton.innerHTML = '\u2261';
-  } else {
-    navClasses.add('displayed');
-    toggleButton.innerHTML = '\u2715';
+  function handleNav(target) {
+    const navClasses = navElement.classList;
+    if (!target || navClasses.contains('displayed')) {
+      navClasses.remove('displayed');
+      toggleButton.innerHTML = '\u2261';
+    } else {
+      navClasses.add('displayed');
+      toggleButton.innerHTML = '\u2715';
+    }
   }
-}
 
-function handleClick(event) {
-  if (navElement.contains(event.target)) {
-    return;
+  function handleClick(event) {
+    if (navElement.contains(event.target)) {
+      return;
+    }
+    handleNav(toggleButton.contains(event.target));
   }
-  handleNav(toggleButton.contains(event.target));
-}
 
-document.addEventListener('touchstart', handleClick);
+  document.addEventListener('touchstart', handleClick);
+  document.addEventListener('click', handleClick);
+});
